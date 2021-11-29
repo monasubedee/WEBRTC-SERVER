1ad0575d3a9e82bc673c656b7c2645029597a7fa
mona <monasubedi.sbd@gmail.com>
2021-11-29 15:21:54 +0630

    initialize the socket server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const { Server } = require('socket.io');
 
 const app = express();
+
+app.use(cors());
+
+const server = require('http').createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:'*',
+        methods:['GET','POST']
+    }
+})
 
 const PORT = process.env.PORT || 5000;
 
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server Listening on port ${PORT}`)
 })
